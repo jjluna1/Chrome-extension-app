6acead6162aa1ea76a514adb05413fe3c25a3532
Jose Luna <jjahaziel1@yahoo.com>
2024-04-17 23:53:46 -0400

    Update index.js
    Made the links entered open in a new tab using template string in link 15-21

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
 let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
-ulEl.innerHTML = listItems
-}
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+     
