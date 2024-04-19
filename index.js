308641fbfbdd323e178dced1249090ccb05b6a67
Jose (J.J.) Luna <99362199+jjluna1@users.noreply.github.com>
2024-04-19 17:49:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+})
 })
 
 function renderLeads() {
@@ -15,4 +22,10 @@ for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
-     
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+ulEl.innerHTML = listItems
+}
+}
